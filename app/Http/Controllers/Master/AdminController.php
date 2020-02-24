db1dfa7b7ffe454dd7fba8f051e02d8ebca8c350
brilyanilhamsadewo <brilyan.ilham9i.36@gmail.com>
2020-02-24 12:05:43 +0700

    up4

---
+++ app/Http/Controllers/Master/AdminController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Master;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AdminController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        echo("Ini halaman index");
+        
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+        echo("Ini halaman Create");
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+        
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        //
+        echo("Ini halaman edit");
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy()
+    {
+        //
+        echo("ini halaman destroy");
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,45 +11,56 @@
 |
 */
 
-Route::get('myView', function () {
-    return view('webtest');
-});
+// Route::get('myView', function () {
+//     return view('webtest');
+// });
 
-Route::get('hi', function () {
-    return "lagi belajar nih ye...";
-});
+// Route::get('hi', function () {
+//     return "lagi belajar nih ye...";
+// });
 
-Route::get('ID/{id}', function ($id) {
-    return 'ID anda : '.$id;
-});
+// Route::get('ID/{id}', function ($id) {
+//     return 'ID anda : '.$id;
+// });
 
-Route::get('viewcontroll', 'MyfirstController@index');
+// Route::get('viewcontroll', 'MyfirstController@index');
 
-Route::get('tampilnama', 'MyfirstController@nama');
+// Route::get('tampilnama', 'MyfirstController@nama');
 
-Route::get('matkul', 'MyFirstController@matkul');
+// Route::get('matkul', 'MyFirstController@matkul');
 
-Route::get('getname/{nama}', 'MyFirstController@getNameFromUrl');
+// Route::get('getname/{nama}', 'MyFirstController@getNameFromUrl');
 
-Route::get('formulir', 'MyFirstController@formulir');
+// Route::get('formulir', 'MyFirstController@formulir');
 
-Route::post('formulir/proses', 'MyFirstController@proses');
+// Route::post('formulir/proses', 'MyFirstController@proses');
 
-Route::get('home', 'MyfirstController@home');
+// Route::get('home', 'MyfirstController@home');
 
-Route::get('tentang', 'MyfirstController@tentang');
+// Route::get('tentang', 'MyfirstController@tentang');
 
-Route::get('admin', function () {
-    return view('admin/admin');
-});
+// Route::get('admin', function () {
+//     return view('admin/admin');
+// });
 
-Route::get('header', function () {
-    return view('admin/header');
-});
+// Route::get('header', function () {
+//     return view('admin/header');
+// });
 
-Route::get('sidebar', function () {
-    return view('admin/sidebar');
-});
-Route::get('footer', function () {
-    return view('admin/footer');
-});
+// Route::get('sidebar', function () {
+//     return view('admin/sidebar');
+// });
+// Route::get('footer', function () {
+//     return view('admin/footer');
+// });
+
+
+Route::get('master/index', 'Master\AdminController@index');
+Route::get('master/create', 'Master\AdminController@create');
+Route::get('master/edit', 'Master\AdminController@edit');
+Route::get('master/destroy', 'Master\AdminController@destroy');
+
+Route::get('Transaksi/index', 'Transaksi\TransaksiController@index');
+Route::get('Transaksi/create', 'Transaksi\TransaksiController@create');
+Route::get('Transaksi/edit', 'Transaksi\TransaksiController@edit');
+Route::get('Transaksi/destroy', 'Transaksi\TransaksiController@destroy');
